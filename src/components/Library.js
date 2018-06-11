1bdd9a90605b7e3b3bd8c6b6dcade6c75fc3218c
Melissa Riggs <melissarocksoc@gmail.com>
2018-06-10 21:50:39 -0700

    components library checkpoint

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
- import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import albumData from './../data/albums';
 
 class Library extends Component { 
@@ -14,11 +14,11 @@ class Library extends Component {
         {
           this.state.albums.map( (album, index) => 
           <Link to={`/album/${album.slug}`} key={index}>
-               <img src={album.albumCover} alt={album.title} />
+          <img src={album.albumCover} alt={album.title} />
                <div>{album.title}</div>
                <div>{album.artist}</div>
                <div>{album.songs.length} songs</div>
-            </Link>
+               </Link>
           )
         }
      </section>
